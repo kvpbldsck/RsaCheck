eb3fa1de2db804695cec23871bdc70c52c12fa17
Valerij Zakharenko <konvpalto87@gmail.com>
2024-07-10 08:55:58 +0200

    Update gitignore and refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
@@ -14,15 +13,6 @@ public class Main {
         var signer = new RsaSigner(privateKey);
         
         System.out.println(signer.makeBase64Signature(message));
-    }
-
-    private static String readMessage() throws IOException {
-        var resource = Main.class.getClassLoader().getResourceAsStream("message.json");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8))) {
-            String content = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-
-            return content;
-        }
     }
     
     private static String readResource(String resourceName) throws Exception {
