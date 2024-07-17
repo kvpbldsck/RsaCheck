2c1a553b19924063b4e7afaeb938dbe19ed40c83
Valerij Zakharenko <konvpalto87@gmail.com>
2024-07-17 15:56:24 +0200

    Provide and improve scripts

--- dotnet/RsaCheck/RsaCheck/run.sh
+++
@@ -1,2 +0,0 @@
-// need to install .NET 9 SDK and ensure it is in the PATH 
-dotnet run
---
+++ scripts/runDotnet.sh
@@ -0,0 +1,3 @@
+# need to install .NET 9 SDK and ensure it is in the PATH 
+cd ../dotnet/RsaCheck/RsaCheck || exit
+dotnet run
